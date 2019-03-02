f67d149f4bc93acca2777760957879972811f62c
Treekay <823334594@qq.com>
2019-03-02 10:56:07 +0800

    sort

--- Algorithm/Search/DFS.cpp
+++ Algorithm/Search/DFS.cpp
@@ -1,9 +1,9 @@
 void dfs(int v) { // recursion version
-  visited[v] = true;
-  cout << v << " ";
-  for (list<int>::iterator iter = graph[v].begin(); iter != graph[v].end(); iter++) {
-    if (!visited[*iter]) {
-      dfs(*iter);
+    visited[v] = true;
+    cout << v << " ";
+    for (list<int>::iterator iter = graph[v].begin(); iter != graph[v].end(); iter++) {
+        if (!visited[*iter]) {
+            dfs(*iter);
+        }
     }
-  }
 }
